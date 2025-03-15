58496b8da359d79e4ecba1d8ed6383ed1de055ed
Toni Wahrstätter <info@toniwahrstaetter.com>
2025-03-15 08:58:17 +0100

    Updated PyXatu core.py for new release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyxatu',
-    version='1.7',
+    version='1.8',
     packages=find_packages(),
     include_package_data=True,
     package_data={
